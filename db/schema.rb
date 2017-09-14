af7c901dcb7535f7f97268e07fad2cf6e6b4ad21
Eugene <skullet-on@mail.ru>
2017-09-14 12:51:32 +0300

    Added Posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914094113) do
+ActiveRecord::Schema.define(version: 20170914094715) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.boolean "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "technologies", force: :cascade do |t|
     t.string "name"
