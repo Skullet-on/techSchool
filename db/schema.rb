e6e33fe662a1cbbe7aa844d8250c198e4e004d53
Eugene <skullet-on@mail.ru>
2017-09-14 12:42:46 +0300

    Added Theme model and pages for working it

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914093625) do
+ActiveRecord::Schema.define(version: 20170914094113) do
 
   create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "themes", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
